94bb64e8bf02c4be556a3b5096874b0bedcead92
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2024-07-05 22:55:26 -0300

    style: added global files

---
+++ src/assets/Style/global.tsx
@@ -0,0 +1,11 @@
+export const STYLE_GUIDE = {
+    color: {
+        primary: '#080A40',
+        secondary: '#836BA9',
+        grayLight: '#F1F1F1',
+        grayDark: '#343434',
+        white: '#fff',
+        
+    },
+    fontFamily: 'Roboto'
+}
