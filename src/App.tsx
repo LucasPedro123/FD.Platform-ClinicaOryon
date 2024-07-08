56b6ad8065ae0e1761fbe9f1749683b3dbd4f850
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2024-07-08 01:02:26 -0300

    feat: added Sidebar to ancestor file

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
+import React from 'react';
 import AppRouter from './Routes/index'
+import { Sidebar } from './Components/Sidebar/Sidebar.component';
 
 
 function App() {
 
   return (
-    <>
+    <div style={{display: 'flex'}}>
+      <Sidebar />
       <AppRouter />
-    </>
+    </div>
   )
 }
 
