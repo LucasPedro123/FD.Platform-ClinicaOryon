f04e48166d3ef9da51c2603fbe65c3663cc48b96
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2024-07-08 01:05:52 -0300

    feat: created page Users

---
+++ src/Pages/Users/Users.page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import * as S from './User.style'
+
+
+export const Users: React.FC = () => {
+    return (
+        <S.UserContainer>
+            Users
+        </S.UserContainer>
+    )
+}
