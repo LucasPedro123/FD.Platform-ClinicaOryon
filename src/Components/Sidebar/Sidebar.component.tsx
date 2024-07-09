16fa78df173225cd7f92d9afda8509b9e799d873
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2024-07-09 17:42:32 -0300

    feat: added interactive style to Sidebar item

--- src/Components/Sidebar/Sidebar.component.tsx
+++ src/Components/Sidebar/Sidebar.component.tsx
@@ -1,9 +1,25 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import * as S from './Sidebar.style'
 import Logo from '../../assets/logo.png'
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom";
+
+export interface IlinkProps {
+    emailLink: boolean;
+    usersLink: boolean;
+}
 
 export const Sidebar: React.FC = () => {
+    const [link, setLink] = useState<IlinkProps>({ emailLink: false, usersLink: false });
+    const location = useLocation();
+
+    useEffect(() => {
+        if (location.pathname === "/Users") {
+            setLink({ emailLink: false, usersLink: true });
+        } else if (location.pathname === "/EmailMarket") {
+            setLink({ emailLink: true, usersLink: false });
+        }
+    }, [location]);
+
     return (
         <S.SidebarContainer>
             <S.SidebarWrapper>
@@ -13,16 +29,16 @@ export const Sidebar: React.FC = () => {
                 </S.SidebarTitle>
             </S.SidebarWrapper>
             <S.SidebarContent>
-                <Link to='/' style={{ textDecoration: 'none' }}>
-                    <S.SidebarItem >
-                        <i className="fa-solid fa-users" style={{ color: 'white', fontSize: '1.3em' }}></i>
-                        <S.SidebarText>Users</S.SidebarText>
+                <Link to='/Users' style={{ textDecoration: 'none' }}>
+                    <S.SidebarItem active={link.usersLink}>
+                        <i className="fa-solid fa-users" style={{ color: link.usersLink ? 'white' : 'black', fontSize: '1.3em' }}></i>
+                        <S.SidebarText active={link.usersLink}>Users</S.SidebarText>
                     </S.SidebarItem>
                 </Link>
                 <Link to='/EmailMarket' style={{ textDecoration: 'none' }}>
-                    <S.SidebarItem >
-                        <i className="fa-regular fa-envelope" style={{ color: 'white', fontSize: '1.3em' }}></i>
-                        <S.SidebarText>Email Market</S.SidebarText>
+                    <S.SidebarItem active={link.emailLink}>
+                        <i className="fa-regular fa-envelope" style={{ color: link.emailLink == true ? 'white' : 'black', fontSize: '1.3em' }}></i>
+                        <S.SidebarText active={link.emailLink}>Email Market</S.SidebarText>
                     </S.SidebarItem>
                 </Link>
             </S.SidebarContent>
