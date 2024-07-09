65a5fea34005722aa43386784f9f875af5ac6c70
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2024-07-09 17:41:38 -0300

    feat: Added new navigation scrollbar

--- src/assets/Style/index.tsx
+++ src/assets/Style/index.tsx
@@ -1,9 +1,24 @@
 import { createGlobalStyle } from 'styled-components';
+import { STYLE_GUIDE } from './global';
 
 export const IndexStyle = createGlobalStyle`
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+
+    
+  }
+
+  ::-webkit-scrollbar{
+      width: 5px;
+  }
+
+  ::-webkit-scrollbar-track{
+      background-color: transparent;
+  }
+
+  ::-webkit-scrollbar-thumb{
+      background-color: ${STYLE_GUIDE.color.secondary} ;
   }
 `;
