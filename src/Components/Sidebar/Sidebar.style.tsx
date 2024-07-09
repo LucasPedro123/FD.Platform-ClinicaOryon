69846c5f746e900656a43ad23c6eb69a05b2ea67
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2024-07-09 17:43:14 -0300

    style: Added fixing to the Sidebar

--- src/Components/Sidebar/Sidebar.style.tsx
+++ src/Components/Sidebar/Sidebar.style.tsx
@@ -1,14 +1,24 @@
 import styled from "styled-components";
 import { STYLE_GUIDE } from "../../assets/Style/global";
+import { IlinkProps } from './Sidebar.component'
+
+interface SidebarItemProps {
+    active: boolean;
+}
+
 
 export const SidebarContainer = styled.div`
-    height: 100vh;
-    width: 223px;
+    position: fixed; 
+    top: 0; 
+    left: 0; 
+    height: 100vh; 
+    width: 223px; 
     background-color: ${STYLE_GUIDE.color.grayLight};
     display: flex;
     flex-direction: column;
     align-items: center;
-`
+    z-index: 1000; 
+`;
 export const SidebarWrapper = styled.div`
     display: flex;
     align-items: center;
@@ -35,23 +45,48 @@ export const SidebarContent = styled.div`
     align-items: start;
 `
 
-export const SidebarItem = styled.div`
-    width: 100%;
-    height: 48px;
-    background-color: ${STYLE_GUIDE.color.primary};
-    border-radius: 10px;
+export const SidebarItem = styled.div<SidebarItemProps>`
+    ${({ active }) => (
+        active ?
+            `
+            width: 100%;
+            height: 48px;
+            background-color: ${STYLE_GUIDE.color.primary};
+            border-radius: 10px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            gap: 12px;
+            padding-inline: 15px;
+        ` :
+            `
+            width: 100%;
+            height: 48px;
+            border-radius: 10px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            gap: 12px;
+            padding-inline: 15px;
+
+            &:hover{
+                background-color:#C4C4C4;
+            }
+        `
+
+    )};
     
-    display: flex;
-    align-items: center;  
-    justify-content: center;  
-    gap: 12px;
-    padding-inline: 15px;
-`
-export const SidebarText = styled.p`
+    transition: 0.3s;
+`;
+export const SidebarText = styled.p<SidebarItemProps>`
     font-family: ${STYLE_GUIDE.fontFamily};
     font-size: 16px;
     font-weight: 800;
-    color: ${STYLE_GUIDE.color.white};
+    color: ${({ active }) => (
+        active ? STYLE_GUIDE.color.white : STYLE_GUIDE.color.dark
+    )
+
+    };
 
 `
 
@@ -65,7 +100,7 @@ export const FooterContent = styled.a`
     display: flex;
     gap: 12px;
     text-decoration: none;
-    color: ${STYLE_GUIDE.color.grayDark}
+    color: ${STYLE_GUIDE.color.grayDark};
 
 `
 export const SidebarFooterText = styled.p`
