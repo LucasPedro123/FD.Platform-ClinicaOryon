3c149da637d2d5ed5f124bfb13cdc5812d5a62c4
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2024-07-13 00:17:07 -0300

    fix: fixed Sidebar rendering on main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import AppRouter from './Routes/index'
 import { Sidebar } from './Components/Sidebar/Sidebar.component';
+import { useLocation } from 'react-router';
 
 
 function App() {
+  const location = useLocation();
 
   return (
-    <div style={{display: 'flex'}}>
-      <Sidebar />
+    <div style={{ display: 'flex' }}>
+      {location.pathname !== '/' ? <Sidebar /> : ''}
       <AppRouter />
     </div>
   )
