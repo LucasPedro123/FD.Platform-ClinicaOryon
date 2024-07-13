32422cb02ee4fd8335d10e4ef0f030a2dc0ff07e
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2024-07-13 00:16:17 -0300

    feat: added authentication page to main route

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
-import {   Routes, Route } from 'react-router-dom';
-import  {Users}  from '../Pages/Users/Users.page';
+import { Routes, Route } from 'react-router-dom';
+import { Users } from '../Pages/Users/Users.page';
 import Email from '../Pages/Email/Email.page';
+import Auth from '../Pages/Auth/Auth.page';
 
 const AppRouter: React.FC = () => {
     return (
-       
-            <Routes>
-                <Route path="/Users"  element={<Users/>} />
-                <Route path="/EmailMarket"  element={<Email/>} />
-            </Routes>
-        
+
+        <Routes>
+            <Route path="/" element={<Auth />} />
+            <Route path="/Users" element={<Users />} />
+            <Route path="/EmailMarket" element={<Email />} />
+        </Routes>
+
     );
 };
 
