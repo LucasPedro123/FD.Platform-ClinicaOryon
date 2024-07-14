d7a741c8c51177fd89295e61985f5ac5be62cfed
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2024-07-14 14:38:18 -0300

    feat:  implemented route protection using LoggedIn context

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -1,18 +1,24 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { Users } from '../Pages/Users/Users.page';
 import Email from '../Pages/Email/Email.page';
 import Auth from '../Pages/Auth/Auth.page';
+import { LoggedIn } from '../Context/LoggedIn.context';
+import {Error} from '../Pages/Error/Error.page';
+
+
 
 const AppRouter: React.FC = () => {
-    return (
+    const context = useContext(LoggedIn);
 
+    return (
         <Routes>
             <Route path="/" element={<Auth />} />
-            <Route path="/Users" element={<Users />} />
-            <Route path="/EmailMarket" element={<Email />} />
+            <Route path="/unauthorized" element={<Error typeError="401"/>} />
+            <Route path="/Users" element={context?.loggedIn ? <Users /> :<Navigate to="/unauthorized" />} />
+            <Route path="/EmailMarket" element={context?.loggedIn ? <Email /> :<Navigate to="/unauthorized" />} />
+            <Route path="*" element={<Error typeError="404"/>} />
         </Routes>
-
     );
 };
 
