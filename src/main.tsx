9b07ab7f710922b403ca8b4e4bd3b27dc37aafa4
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2024-07-13 22:28:46 -0300

    feat: added providing context for the main file

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { IndexStyle } from './assets/Style/index.tsx'
 import { BrowserRouter as Router } from 'react-router-dom'
+import { LoggedInProvider } from './Context/LoggedIn.context.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <IndexStyle />
-    <Router>
-      <App />
-    </Router>
+    <LoggedInProvider>
+      <IndexStyle />
+      <Router>
+        <App />
+      </Router>
+    </LoggedInProvider>
   </React.StrictMode>,
 )
