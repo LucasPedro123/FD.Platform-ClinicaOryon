9cb768711be8248748f4b55b53558577747979e9
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2024-07-14 21:14:46 -0300

    feat: created interfaces

---
+++ src/Interfaces/web.interfaces.tsx
@@ -0,0 +1,30 @@
+
+
+export interface IUser {
+    email: string;
+    name: string;
+    password: string;
+    phone: string;
+    photoURL: string;
+    userId: string;
+    weeklyCalories?: number;
+}
+
+export interface Food {
+    name: string;
+    portion: string;
+    calories: number;
+}
+
+export interface FoodContextType {
+    foodItems: Food[];
+    searchTerm: string;
+    setSearchTerm: (term: string) => void;
+    fetchFoodItems: () => void;
+}
+
+export interface UserContextType {
+    users: IUser[];
+    userImages: { [key: string]: string };
+    fetchUsers: () => void;
+}
