ed8741dc7dc3901c776672a8785bc2a5c0a63863
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2024-07-14 21:17:12 -0300

    feat: added user context to Users page

--- src/Pages/Users/Users.page.tsx
+++ src/Pages/Users/Users.page.tsx
@@ -1,114 +1,35 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import * as S from './User.style';
-import { db, storage, auth } from '../../Services/fireConfig';
-import { collection, getDocs, query, where, Timestamp, deleteDoc, doc } from "firebase/firestore";
-import { ref, getDownloadURL, deleteObject } from "firebase/storage";
+import { db, auth, storage } from '../../Services/fireConfig';
+import { deleteDoc, doc } from "firebase/firestore";
+import { ref, deleteObject } from "firebase/storage";
 import { deleteUser } from "firebase/auth";
 import iconUser from '../../assets/icon-user.png';
 import Modal from 'react-modal';
+import { useUserContext } from '../../Context/Users.context';
+import { IUser } from "../../Interfaces/web.interfaces";
 
-
-interface User {
-    email: string;
-    name: string;
-    password: string;
-    phone: string;
-    photoURL: string;
-    userId: string;
-    weeklyCalories?: number; // Adicionando a propriedade para as calorias semanais
-}
-
-// Define the root element for the modal
 Modal.setAppElement('#root');
 
 export const Users: React.FC = () => {
-    const [users, setUsers] = useState<User[]>([]);
-    const [userImages, setUserImages] = useState<{ [key: string]: string }>({});
+    const { users, userImages, fetchUsers } = useUserContext();
     const [showModal, setShowModal] = useState(false);
-    const [selectedUser, setSelectedUser] = useState<User | null>(null);
-
-    useEffect(() => {
-        const getUsersFromFirestore = async () => {
-            try {
-                const usersCollection = await getDocs(collection(db, 'users'));
-                const usersData: User[] = [];
-
-                const today = new Date();
-                const startOfWeek = new Date(today);
-                startOfWeek.setDate(today.getDate() - today.getDay());
-                startOfWeek.setHours(0, 0, 0, 0);
-
-                const endOfWeek = new Date(today);
-                endOfWeek.setDate(today.getDate() + (6 - today.getDay()));
-                endOfWeek.setHours(23, 59, 59, 999);
-
-                for (const doc of usersCollection.docs) {
-                    const userData = doc.data() as User;
-                    // Verificação se todos os campos estão preenchidos
-                    if (userData.email && userData.name && userData.password && userData.phone) {
-                        const foodsCollectionRef = collection(db, `users/${userData.userId}/foods`);
-                        const foodQuery = query(
-                            foodsCollectionRef,
-                            where('date', '>=', Timestamp.fromDate(startOfWeek)),
-                            where('date', '<=', Timestamp.fromDate(endOfWeek))
-                        );
-
-                        const foodSnapshot = await getDocs(foodQuery);
-                        const foods = foodSnapshot.docs.map(doc => doc.data());
-
-                        const totalCalories = foods.reduce((total, food: any) => total + food.calories, 0);
-                        userData.weeklyCalories = totalCalories;
-
-                        usersData.push(userData);
-                    }
-                }
-
-                setUsers(usersData);
-                await fetchUserImages(usersData);
-            } catch (error) {
-                console.error('Error fetching users:', error);
-            }
-        };
-
-        const fetchUserImages = async (users: User[]) => {
-            const images: { [key: string]: string } = {};
-            for (const user of users) {
-                try {
-                    const imageRef = ref(storage, `profile_pictures/${user.userId}.jpg`);
-                    const imageUrl = await getDownloadURL(imageRef);
-                    if (imageUrl) {
-                        images[user.userId] = imageUrl;
-                    }
-                } catch (error) {
-                    console.error(`Error fetching image for user ${user.userId}:`, error);
-                }
-            }
-            setUserImages(images);
-        };
-
-        getUsersFromFirestore();
-        console.log("Imagem do usuário:",userImages)
-    }, []);
+    const [selectedUser, setSelectedUser] = useState<IUser | null>(null);
 
     const handleDeleteUser = async () => {
         if (selectedUser) {
             try {
-                // Remover o usuário do Firebase Authentication
                 const userAuth = auth.currentUser;
-                console.log(userAuth)
                 if (userAuth) {
                     await deleteUser(userAuth);
                 }
 
-                // Remover o usuário do Firestore
                 await deleteDoc(doc(db, 'users', selectedUser.userId));
 
-                // Remover a imagem do usuário do Firebase Storage
                 const imageRef = ref(storage, `profile_pictures/${selectedUser.userId}.jpg`);
                 await deleteObject(imageRef);
 
-                // Atualizar o estado para remover o usuário da lista
-                setUsers(users.filter(user => user.userId !== selectedUser.userId));
+                fetchUsers(); // Atualizar a lista de usuários
                 setShowModal(false);
                 setSelectedUser(null);
             } catch (error) {
@@ -150,7 +71,7 @@ export const Users: React.FC = () => {
                             <S.Value>{user.email}</S.Value>
                             <S.Value>{user.phone}</S.Value>
                             <S.Value>{user.password}</S.Value>
-                            <S.Value>{user.weeklyCalories?.toFixed(2)}</S.Value> {/* Exibindo as calorias semanais */}
+                            <S.Value>{user.weeklyCalories?.toFixed(2)}</S.Value>
                             <S.Actions>
                                 <button onClick={() => { setSelectedUser(user); setShowModal(true); }}>⋮</button>
                             </S.Actions>
