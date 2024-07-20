92d31e7c0f7c47b197c19648301b7679cb0fdffe
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2024-07-19 23:13:00 -0300

    feat: created parameterized route for each email

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -4,10 +4,9 @@ import { Users } from '../Pages/Users/Users.page';
 import Email from '../Pages/Email/Email.page';
 import Auth from '../Pages/Auth/Auth.page';
 import { LoggedIn } from '../Context/LoggedIn.context';
-import {Error} from '../Pages/Error/Error.page';
+import { Error } from '../Pages/Error/Error.page';
 import { Database } from '../Pages/Database/Database.page';
-
-
+import { EmailDetails } from '../Pages/EmailDetals/EmailDetails.page';
 
 const AppRouter: React.FC = () => {
     const context = useContext(LoggedIn);
@@ -17,7 +16,8 @@ const AppRouter: React.FC = () => {
             <Route path="/" element={<Auth />} />
             <Route path="/unauthorized" element={<Error typeError="401"/>} />
             <Route path="/Users" element={context?.loggedIn ? <Users /> :<Navigate to="/unauthorized" />} />
-            <Route path="/EmailMarket" element={context?.loggedIn ? <Email /> :<Navigate to="/unauthorized" />} />
+            <Route path="/EmailMarketing" element={context?.loggedIn ? <Email /> :<Navigate to="/unauthorized" />} />
+            <Route path="/EmailMarketing/:id" element={context?.loggedIn ? <EmailDetails /> : <Navigate to="/unauthorized" />} /> {/* Nova rota parametrizada */}
             <Route path="/Database" element={context?.loggedIn ? <Database /> :<Navigate to="/unauthorized" />} />
             <Route path="*" element={<Error typeError="404"/>} />
         </Routes>
