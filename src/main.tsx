61849216027342ddd7cc44f0517287b78f513790
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2024-07-19 23:10:45 -0300

    feat: added providing EmailSend context

--- src/main.tsx
+++ src/main.tsx
@@ -6,18 +6,21 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import { LoggedInProvider } from './Context/LoggedIn.context.tsx'
 import { UserProvider } from './Context/Users.context.tsx'
 import { FoodProvider } from './Context/Foods.context.tsx'
+import { EmailSendProvider } from './Context/EmailsSend.context.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <FoodProvider>
-      <UserProvider>
-        <LoggedInProvider>
-          <IndexStyle />
-          <Router>
-            <App />
-          </Router>
-        </LoggedInProvider>
-      </UserProvider>
-    </FoodProvider>
+    <EmailSendProvider>
+      <FoodProvider>
+        <UserProvider>
+          <LoggedInProvider>
+            <IndexStyle />
+            <Router>
+              <App />
+            </Router>
+          </LoggedInProvider>
+        </UserProvider>
+      </FoodProvider>
+    </EmailSendProvider>
   </React.StrictMode>,
 )
