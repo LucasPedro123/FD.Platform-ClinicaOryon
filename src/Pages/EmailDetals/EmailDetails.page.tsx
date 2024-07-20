f695a9563678741e314655b52dbc1d69c5f638ba
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2024-07-19 23:09:13 -0300

    feat: created page EmailDetails

---
+++ src/Pages/EmailDetals/EmailDetails.page.tsx
@@ -0,0 +1,19 @@
+import React, { useContext } from 'react';
+import { useParams } from 'react-router-dom'; // Importar o hook para acessar os parâmetros da rota
+import * as S from './EmailDetails.style'; // Importe o módulo de estilos conforme necessário
+import { EmailSendContext } from '../../Context/EmailsSend.context';
+
+
+
+export const EmailDetails: React.FC = () => {
+    const { id } = useParams<{ id: string  }>(); 
+    const context = useContext(EmailSendContext);
+    const EmailDetail = id ? context?.emailSend.find(e => e.id == parseInt(id)) : undefined;
+
+    const ComponentEmail = EmailDetail?.component;
+    return (
+        <S.Container>
+            {ComponentEmail && <ComponentEmail />}
+        </S.Container>
+    );
+};
