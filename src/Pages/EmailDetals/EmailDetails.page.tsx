f635477b0747095bb1ea1b14a191a2c7ffc307c2
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2024-07-21 23:47:56 -0300

    feat: added conditional to EmailDetails component

--- src/Pages/EmailDetals/EmailDetails.page.tsx
+++ src/Pages/EmailDetals/EmailDetails.page.tsx
@@ -3,17 +3,16 @@ import { useParams } from 'react-router-dom'; // Importar o hook para acessar os
 import * as S from './EmailDetails.style'; // Importe o módulo de estilos conforme necessário
 import { EmailSendContext } from '../../Context/EmailsSend.context';
 
-
-
 export const EmailDetails: React.FC = () => {
-    const { id } = useParams<{ id: string  }>(); 
+    const { id } = useParams<{ id: string }>(); 
     const context = useContext(EmailSendContext);
-    const EmailDetail = id ? context?.emailSend.find(e => e.id == parseInt(id)) : undefined;
+    const EmailDetail = id ? context?.emailSend.find(e => e.id === parseInt(id)) : undefined;
 
     const ComponentEmail = EmailDetail?.component;
+    
     return (
         <S.Container>
-            {ComponentEmail && <ComponentEmail />}
+            {ComponentEmail ? <ComponentEmail /> : <div>Componente não encontrado</div>}
         </S.Container>
     );
 };
