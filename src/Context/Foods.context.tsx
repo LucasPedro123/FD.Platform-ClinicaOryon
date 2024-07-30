f3c33886234638cdf01d55f466ef8dee99856dd8
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2024-07-30 08:19:51 -0300

    fix: changed food base for server search

--- src/Context/Foods.context.tsx
+++ src/Context/Foods.context.tsx
@@ -1,7 +1,7 @@
-import React, { createContext, useContext, useState, useEffect, ReactNode } from 'react';
-import { collection, getDocs, addDoc } from 'firebase/firestore';
-import { db } from '../Services/fireConfig';
+import React, { createContext, useContext, useState, useEffect, ReactNode,  useMemo } from 'react';
+import axios from 'axios';
 import { Food, FoodContextType } from '../Interfaces/web.interfaces';
+import { toast } from 'react-toastify';
 
 const FoodContext = createContext<FoodContextType | undefined>(undefined);
 
@@ -11,53 +11,81 @@ const FoodProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
     const [searchTerm, setSearchTerm] = useState('');
 
     const fetchFoodItems = async () => {
-        const foodCollection = collection(db, 'foodItems');
-        const querySnapshot = await getDocs(foodCollection);
-        const foods = querySnapshot.docs.map(doc => ({ ...doc.data(), firestoreId: doc.id }) as Food);
-        setAllFoodItems(foods);
-        setFoodItems(foods);
-        console.log(`Número de alimentos: ${foods.length}`);
+        try {
+            const response = await axios.get('https://fooddatajson.onrender.com/api/foods');
+            setAllFoodItems(response.data);
+        } catch (error) {
+            console.error('Error fetching food items:', error);
+        }
     };
 
     useEffect(() => {
         fetchFoodItems();
     }, []);
 
-    useEffect(() => {
+    const filteredFoods = useMemo(() => {
         if (searchTerm) {
-            const filteredFoods = allFoodItems.filter(food =>
+            return allFoodItems.filter(food =>
                 food.name.toLowerCase().includes(searchTerm.toLowerCase())
             );
-            setFoodItems(filteredFoods);
-        } else {
-            setFoodItems(allFoodItems);
         }
+        return allFoodItems;
     }, [searchTerm, allFoodItems]);
+
+    useEffect(() => {
+        setFoodItems(filteredFoods);
+    }, [filteredFoods]);
 
     const addFoodItem = async (newFood: Food) => {
         try {
-            const docRef = await addDoc(collection(db, 'foodItems'), newFood);
-            const foodWithId = { ...newFood, firestoreId: docRef.id };
+            // Adiciona o alimento no banco de dados do servidor
+            const response = await axios.post('https://fooddatajson.onrender.com/api/foods', newFood);
+            const foodWithId = { ...newFood, id: response.data._id };
+    
+            // Atualiza o estado local com o novo alimento
             setAllFoodItems(prevFoods => [...prevFoods, foodWithId]);
             setFoodItems(prevFoods => [...prevFoods, foodWithId]);
         } catch (error) {
             console.error('Error adding document: ', error);
         }
     };
+    
 
-    const updateFoodItem = (updatedFood: Food) => {
-        setAllFoodItems(prevFoods =>
-            prevFoods.map(food => (food.firestoreId === updatedFood.firestoreId ? updatedFood : food))
-        );
-        setFoodItems(prevFoods =>
-            prevFoods.map(food => (food.firestoreId === updatedFood.firestoreId ? updatedFood : food))
-        );
-    };
+    const updateFoodItem = async (updatedFood: Food) => {
+        try {
+            // Atualiza o alimento no banco de dados do servidor
+            await axios.put(`https://fooddatajson.onrender.com/api/foods/${updatedFood.id}`, updatedFood);
+            
+            console.log(updatedFood)
+            // Atualiza o estado local com o alimento atualizado
+            setAllFoodItems(prevFoods =>
+                prevFoods.map(food => (food.id === updatedFood.id ? updatedFood : food))
+            );
+            setFoodItems(prevFoods =>
+                prevFoods.map(food => (food.id === updatedFood.id ? updatedFood : food))
+            );
 
-    const deleteFoodItem = (foodId: string | undefined) => {
-        setAllFoodItems(prevFoods => prevFoods.filter(food => food.firestoreId !== foodId));
-        setFoodItems(prevFoods => prevFoods.filter(food => food.firestoreId !== foodId));
+            toast.success('Food item updated successfully!');
+
+        } catch (error) {
+            console.error('Error updating document: ', error);
+        }
     };
+    
+
+    const deleteFoodItem = async (foodId:number ) => {
+        try {
+            // Remove o alimento do banco de dados do servidor
+            await axios.delete(`https://fooddatajson.onrender.com/api/foods/${foodId}`);
+    
+            // Atualiza o estado local removendo o alimento
+            setAllFoodItems(prevFoods => prevFoods.filter(food => food._id !== foodId));
+            setFoodItems(prevFoods => prevFoods.filter(food => food._id !== foodId));
+        } catch (error) {
+            console.error('Error deleting document: ', error);
+        }
+    };
+    
 
     return (
         <FoodContext.Provider value={{ foodItems, searchTerm, setSearchTerm, fetchFoodItems, addFoodItem, updateFoodItem, deleteFoodItem }}>
