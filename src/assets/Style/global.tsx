0162b8cf3f81e12597102172d108021437b44a92
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2024-08-13 09:01:47 -0300

    chore: added two gray color palette

--- src/assets/Style/global.tsx
+++ src/assets/Style/global.tsx
@@ -5,8 +5,9 @@ export const STYLE_GUIDE = {
         grayLight: '#F1F1F1',
         grayDark: '#343434',
         white: '#fff',
-        dark: '#000000'
-        
+        dark: '#000000',
+        gray100: '#666666',
+        gray200: '#7D7D7D'
     },
     fontFamily: 'Roboto'
 }
