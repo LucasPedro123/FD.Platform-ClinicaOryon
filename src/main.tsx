4a05d9d4725a641036f768ca03320689f11a1a23
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2024-08-13 08:50:41 -0300

    feat: added NotificationsProvider in Main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -7,20 +7,23 @@ import { LoggedInProvider } from './Context/LoggedIn.context.tsx'
 import { UserProvider } from './Context/Users.context.tsx'
 import { FoodProvider } from './Context/Foods.context.tsx'
 import { EmailSendProvider } from './Context/EmailsSend.context.tsx'
+import { NotificationsProvider } from './Context/Notifications.context.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <EmailSendProvider>
-      <FoodProvider>
-        <UserProvider>
-          <LoggedInProvider>
-            <IndexStyle />
-            <Router>
-              <App />
-            </Router>
-          </LoggedInProvider>
-        </UserProvider>
-      </FoodProvider>
-    </EmailSendProvider>
-  </React.StrictMode>,
+    <NotificationsProvider>
+      <EmailSendProvider>
+        <FoodProvider>
+          <UserProvider>
+            <LoggedInProvider>
+              <IndexStyle />
+              <Router>
+                <App />
+              </Router>
+            </LoggedInProvider>
+          </UserProvider>
+        </FoodProvider>
+      </EmailSendProvider>
+    </NotificationsProvider>
+  </React.StrictMode>
 )
