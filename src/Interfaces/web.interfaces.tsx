1abe21e385c985881a7532f09a8aa9ed0e8a1f86
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2025-02-28 09:03:22 -0300

    chore: added types

--- src/Interfaces/web.interfaces.tsx
+++ src/Interfaces/web.interfaces.tsx
@@ -1,76 +1,89 @@
-
-
 export interface IUser {
-    email: string;
-    name: string;
-    password: string;
-    phone: string;
-    photoURL: string;
-    userId: string;
-    birthDate?: string;
-    weeklyCalories?: number;
-    firestoreId?: string | undefined;
+  email: string;
+  name: string;
+  password: string;
+  phone: string;
+  photoURL: string;
+  userId: string;
+  birthDate?: string;
+  weeklyCalories?: number;
+  firestoreId?: string | undefined;
 }
 
 export interface Food {
-    _id?: any;
-    id: number;
-    name: string;
-    portion: string;
-    calories: number;
-    firestoreId?: string; 
+  _id?: number;
+  id: number;
+  name: string;
+  portion: string;
+  calories: number;
+  firestoreId?: string;
 }
 
+export interface Category {
+  id?: number;
+  category: string;
+  items: Food[];
+}
 
 export interface FoodContextType {
-    foodItems: Food[];
-    searchTerm: string;
-    setSearchTerm: (term: string) => void;
-    fetchFoodItems: () => void;
-    updateFoodItem: (updatedFood: Food) => void;
-    deleteFoodItem: (foodId: number) => void;
-    addFoodItem: (food: Food) => void;
+  categories: string[] | undefined;
+  foodItems: Food[];
+  searchTerm: string;
+  setSearchTerm: (term: string) => void;
+  fetchCategories: () => void;
+  fetchFoodItems: (category: string) => void;
+  updateFoodItem: (category: string, updatedFood: Food) => void;
+  deleteFoodItem: (category: string, foodId: number) => void;
+  addFoodItem: (category: string, food: Food) => void;
+  addCategory: (newCategory: Category) => Promise<void>;
+  updateCategory: (categoryId: string, updatedCategory: Category) => void;
+  deleteCategory: (category: string) => Promise<void>;
 }
 
 export interface UserContextType {
-    users: IUser[];
-    userImages: { [key: string]: string };
-    fetchUsers: () => void;
-    deleteUserAccount: (user: IUser) => Promise<void>;
-    getUserWeeklyCalories?: (userId: string) => Promise<number>;
-    getUserDailyCalories?:(userId: string) => Promise<{ date: string; calories: number; }[]>;
+  users: IUser[];
+  userImages: { [key: string]: string };
+  fetchUsers: () => void;
+  deleteUserAccount: (user: IUser) => Promise<void>;
+  getUserWeeklyCalories?: (userId: string) => Promise<number>;
+  getUserDailyCalories?: (
+    userId: string
+  ) => Promise<{ date: string; calories: number }[]>;
 }
 
 export interface IEmailSend {
-    emailSend: IEmailProps[]
+  emailSend: IEmailProps[];
 }
 
 export interface IEmailProps {
-    id: number;
-    image: string;
-    company: string;
-    title: string;
-    label: string;
-    component: () => JSX.Element; // Sem parâmetros
+  id: number;
+  image: string;
+  company: string;
+  title: string;
+  label: string;
+  component: () => JSX.Element; // Sem parâmetros
 }
 export interface Notification {
-    id: string;
-    title: string;
-    content: string;
-    date: string;
+  id: string;
+  title: string;
+  content: string;
+  date: string;
 }
 export interface NotificationsContextProps {
-    notifications: Notification[];
-    loading: boolean;
-    fetchNotifications: () => void;
-    addNotification: (notification: Omit<Notification, 'id'>) => void;
-    editNotification: (id: string, updatedNotification: Omit<Notification, 'id'>) => void;
-    deleteNotification: (id: string) => void;
+  notifications: Notification[];
+  loading: boolean;
+  fetchNotifications: () => void;
+  addNotification: (notification: Omit<Notification, "id">) => void;
+  editNotification: (
+    id: string,
+    updatedNotification: Omit<Notification, "id">
+  ) => void;
+  deleteNotification: (id: string) => void;
 }
 
 export interface IlinkProps {
-    emailLink: boolean;
-    usersLink: boolean;
-    databaseLink: boolean;
-    notificationsLink: boolean;
+  emailLink: boolean;
+  usersLink: boolean;
+  databaseLink: boolean;
+  notificationsLink: boolean;
 }
