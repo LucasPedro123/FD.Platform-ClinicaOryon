c2327c031d61ef2c95712a04abc034e1496444b0
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2025-03-04 21:02:48 -0300

    refactor(global): added code formatting

--- src/assets/Style/global.tsx
+++ src/assets/Style/global.tsx
@@ -1,14 +1,15 @@
 export const STYLE_GUIDE = {
-    color: {
-        alert: '#FF4D4D',
-        primary: '#080A40',
-        secondary: '#836BA9',
-        grayLight: '#F1F1F1',
-        grayDark: '#343434',
-        white: '#fff',
-        dark: '#000000',
-        gray100: '#666666',
-        gray200: '#7D7D7D'
-    },
-    fontFamily: 'Roboto'
-}
+  color: {
+    alert: "#FF4D4D",
+    primary: "#080A40",
+    secondary: "#836BA9",
+    grayLight: "#F1F1F1",
+    grayDark: "#343434",
+    white: "#fff",
+    dark: "#000000",
+    gray100: "#666666",
+    gray200: "#7D7D7D",
+    gray300: "#c4c4c4",
+  },
+  fontFamily: "Roboto",
+};
